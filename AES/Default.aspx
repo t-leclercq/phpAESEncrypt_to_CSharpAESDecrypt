86a4d3ee835ec09fa465883fd62355aada30da2d
Thomas Leclercq <t.leclercq@b0xx.dev>
2020-09-21 17:25:26 +0200

    Ajoutez des fichiers projet.

---
+++ AES/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AES._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h2>PHP => C# | AES Chiffrage/Déchiffrage</h2>
+        <br />
+        <p class="lead" runat="server">Voici le code provenant de la page PHP, déchiffré par la page C# :</p>
+        <p><strong><asp:Label ID="CodeDechiffre" runat="server"></asp:Label></strong></p>
+    </div>
+
+</asp:Content>
---
+++ AES/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.UI;
+
+namespace AES
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CodeDechiffre.Text = Decrypt(DownloadString("http://aes/chiffrer.php"), "e835529c2a196ee1026d6c1a7d77f28c");
+            //CodeDechiffre.Text = DownloadString("http://aes/chiffrer.php");
+        }
+        public String Decrypt(String text, String key)
+        {
+            //decode cipher text from base64
+            byte[] cipher = Convert.FromBase64String(text);
+            //get key bytes
+            byte[] btkey = Encoding.ASCII.GetBytes(key);
+
+            //init AES 256
+            RijndaelManaged aes256 = new RijndaelManaged
+            {
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.Zeros,
+        };
+
+            //decrypt
+            ICryptoTransform decryptor = aes256.CreateDecryptor(btkey, null);
+            MemoryStream ms = new MemoryStream(cipher);
+            CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+
+            byte[] plain = new byte[cipher.Length];
+            int decryptcount = cs.Read(plain, 0, plain.Length);
+
+            ms.Close();
+            cs.Close();
+
+            //return plaintext in String
+            return Encoding.UTF8.GetString(plain, 0, decryptcount);
+        }
+
+        public static string DownloadString(string address)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(address);
+
+            return reply;
+        }
+
+    }
+}
